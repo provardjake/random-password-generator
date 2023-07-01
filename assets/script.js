4be1166cf4282642974d141f36fc22785ebf8f73
Jake Provard <provardjake@gmail.com>
2023-07-01 16:20:47 -0600

    added prompts to choose length of the password

--- assets/script.js
+++ assets/script.js
@@ -13,5 +13,26 @@ function writePassword() {
 
 }
 
+function generatePassword(){
+  
+  var passLength = prompt("Please enter the length of your password between 8 and 128: ");
+  if(!passLength){
+    return;
+  }
+  passLength = parseInt(passLength);
+  console.log(passLength);
+
+   if (Number.isInteger(passLength) === false){
+     alert("Please enter a number");
+     generatePassword();
+  }
+  if (passLength < 8 || passLength > 128){
+    alert("Please enter a number between 8 and 128");
+    generatePassword();
+  }
+
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
