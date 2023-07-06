8a55da57fe838dde9b0e6d82d904df5a3221c319
Jake Provard <provardjake@gmail.com>
2023-07-05 19:29:25 -0600

    added random number fucntion and code to check if the random number correlates to the users input

--- assets/script.js
+++ assets/script.js
@@ -13,18 +13,34 @@ function writePassword() {
 
 }
 
+//Generates a random number between two given numbers. 
+function getRndInt(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+
 function generatePassword(){
   var isUpperCase;
   var isLowerCase;
   var isNumeric;
   var isSpecial;
+  var password;
+  var rdmCharType;
+  var passArray = [];
+  var charTypes = [];
+  var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
+  var specialChar = "`~!@#$%^&*()-_=+[{]};:,<.>/?";
+
   var passLength = prompt("Please enter the length of your password between 8 and 128: ");
+
+  //ends the function if the user selects cancel
   if(!passLength){
     return;
+
   }
   //changes the input from the prompt from a string to an integer
   passLength = parseInt(passLength);
-  console.log(passLength);
 
   //checks if passLength is a number, if it isn't, asks to enter a number and prompts again. 
   if (Number.isInteger(passLength) === false){
@@ -33,7 +49,7 @@ function generatePassword(){
 
   }
 
-  //Checks that the password length is between 8 and 12 charaters
+  //Checks that the password length is between 8 and 128 charaters
   if (passLength < 8 || passLength > 128){
     alert("Please enter a number between 8 and 128");
     generatePassword();
@@ -45,22 +61,42 @@ function generatePassword(){
   isSpecial = confirm("Would you like your password to contain special charaters?");
 
   //Checks to make sure at least one character type is selected. 
-for(var i = 0; i < 1;){
-  if (!isUpperCase && !isLowerCase && !isNumeric && !isSpecial){
-    alert("Please select at least one character type for you password.")
-    isUpperCase = confirm("Would you like your password to contain upper case letters?");
-    isLowerCase = confirm("Would you like your password to contain lower case letters?");
-    isNumeric = confirm("Would you like your password to contain numbers?");
-    isSpecial = confirm("Would you like your password to contain special charaters?");
-   }
-   else{
-    i++;
-   }
-}
+  for(var i = 0; i < 1;){
+    if (!isUpperCase && !isLowerCase && !isNumeric && !isSpecial){
+      alert("Please select at least one character type for you password.")
+      isUpperCase = confirm("Would you like your password to contain upper case letters?");
+      isLowerCase = confirm("Would you like your password to contain lower case letters?");
+      isNumeric = confirm("Would you like your password to contain numbers?");
+      isSpecial = confirm("Would you like your password to contain special charaters?");
+     }
+    else{
+     i++;
+    }
+  }
+  charTypes = [isUpperCase, isLowerCase, isNumeric, isSpecial];
 
+  rdmCharType = getRndInt(0, charTypes.length);
 
+  //checks if the generated number is correlated with the character type was selected by the user. If it isn't, gets a new random number
+  for (var i = 0; i < 1;){
+    if(rdmCharType === 0 && charTypes[0] === false){
+      rdmCharType = getRndInt(0, charTypes.length);
+    }
+    else if(rdmCharType === 1 && charTypes[1] === false){
+      rdmCharType = getRndInt(0, charTypes.length);
+    }
+    else if(rdmCharType === 2 && charTypes[2] === false){
+      rdmCharType = getRndInt(0, charTypes.length);
+    }
+    else if(rdmCharType === 3 && charTypes[3] === false){
+      rdmCharType = getRndInt(0, charTypes.length);
+    }
+    else{
+      i++;
+    }
+  }
 
-
+  
 }
 
 // Add event listener to generate button
