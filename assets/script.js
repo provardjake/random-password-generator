d890f6f2ee08c8b20d510c81aedd7479b7d4a63e
Jake Provard <provardjake@gmail.com>
2023-07-05 21:51:03 -0600

    added code to select a random charater for each character in the password

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,7 @@ function writePassword() {
 }
 
 //Generates a random number between two given numbers. 
-function getRndInt(min, max) {
+function getRdmInt(min, max) {
   return Math.floor(Math.random() * (max - min) ) + min;
 }
 
@@ -31,6 +31,12 @@ function generatePassword(){
   var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
   var specialChar = "`~!@#$%^&*()-_=+[{]};:,<.>/?";
+  var upperCaseCharArray = upperCaseChar.split("");
+  var lowerCaseCharArray = lowerCaseChar.split("");
+  var specialCharArray = specialChar.split("");
+  var rdmUpperCase;
+  var rdmLowerCase;
+  var rdmSpecial;
 
   var passLength = prompt("Please enter the length of your password between 8 and 128: ");
 
@@ -75,27 +81,50 @@ function generatePassword(){
   }
   charTypes = [isUpperCase, isLowerCase, isNumeric, isSpecial];
 
-  rdmCharType = getRndInt(0, charTypes.length);
 
-  //checks if the generated number is correlated with the character type was selected by the user. If it isn't, gets a new random number
-  for (var i = 0; i < 1;){
-    if(rdmCharType === 0 && charTypes[0] === false){
-      rdmCharType = getRndInt(0, charTypes.length);
+
+  for(var i = 0; i < passLength; i++){
+    rdmCharType = getRdmInt(0, charTypes.length);
+    //checks if the generated number is correlated with the character type was selected by the user. If it isn't, gets a new random number
+    for (var x = 0; x < 1;){
+      if(rdmCharType === 0 && charTypes[0] === false){
+        rdmCharType = getRdmInt(0, charTypes.length);
+      }
+      else if(rdmCharType === 1 && charTypes[1] === false){
+        rdmCharType = getRdmInt(0, charTypes.length);
+      }
+      else if(rdmCharType === 2 && charTypes[2] === false){
+        rdmCharType = getRdmInt(0, charTypes.length);
+      }
+      else if(rdmCharType === 3 && charTypes[3] === false){
+      rdmCharType = getRdmInt(0, charTypes.length);
+      }
+      else{
+      x++;
+      }
+  }
+    //adds a random character to the password for each the character types that was selected by the user
+    if(rdmCharType === 0){
+      rdmUpperCase = getRdmInt(0, upperCaseCharArray.length);
+      passArray[i] = upperCaseCharArray[rdmUpperCase];
     }
-    else if(rdmCharType === 1 && charTypes[1] === false){
-      rdmCharType = getRndInt(0, charTypes.length);
+    else if(rdmCharType === 1){
+      rdmLowerCase = getRdmInt(0, lowerCaseCharArray.length);
+      passArray[i] = lowerCaseCharArray[rdmLowerCase];
     }
-    else if(rdmCharType === 2 && charTypes[2] === false){
-      rdmCharType = getRndInt(0, charTypes.length);
+    else if(rdmCharType === 2){
+      passArray[i] = getRdmInt(0, 9);
     }
-    else if(rdmCharType === 3 && charTypes[3] === false){
-      rdmCharType = getRndInt(0, charTypes.length);
-    }
-    else{
-      i++;
+    else if(rdmCharType === 3){
+      rdmSpecial = getRdmInt(0, specialCharArray.length);
+      passArray[i] = specialCharArray[rdmSpecial];
     }
   }
 
+  
+  password = passArray.join("");
+  alert("Your password is: \n\n"+password);
+  return password;
   
 }
 
